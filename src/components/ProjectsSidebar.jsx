5eca782908de1232a11d5b3ea95445936e593e5d
renata tsedenova <recedenova@gmail.com>
2024-03-11 22:27:29 +0300

    add input,button,noprojectselected components

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,18 +1,21 @@
 export default function ProjectsSidebar(){
-    return <aside className="w-1/3 
+    return <aside className="
+    w-1/3 
     px-8 py-16 
     bg-stone-900 
     text-stone-50
     md:w-72
     rounded-r-xl"
     >
-        <h2 className="mb-8
+        <h2 className="
+        mb-8
         font-bold
         uppercase
         md:text-xl
         text-stone-200">Your projects</h2>
         <div>
-            <button className="px-4
+            <button className="
+            px-4
             py-2
             text-xs
             md:text-base
