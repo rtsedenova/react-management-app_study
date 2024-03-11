880be160125c7670237b02b9d4ac6206433b8a2a
renata tsedenova <recedenova@gmail.com>
2024-03-11 21:51:37 +0300

    initial commit2

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,8 @@
+export default function NewProject(){
+    return <div>
+        <menu>
+            <li><button>Cancel</button></li>
+            <li><button>Save</button></li>
+        </menu>
+    </div>
+}
